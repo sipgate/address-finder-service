da199c6ae41f9b73d8385f70bccf245359b2e749
Felix Gladisch <felix@felixgladisch.de>
2018-03-29 10:31:07 +0200

    feat: rate limiter

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import express = require("express");
+import RateLimit = require("express-rate-limit");
 
 import handleAddressRequest from "./address.controller";
 import Address from "./address.model";
 import cors from "./cors.middleware";
 
+const limiter: express.RequestHandler = new RateLimit({
+  delayMs: 0, // disable delaying - full speed until the max limit is reached
+  max: 10, // limit each IP to 10 requests per minute
+  windowMs: 60000 // 1 minute
+});
+
 const PORT: number = Number(process.env.PORT) || 8080;
 
 const app: express.Application = express();
 
+app.enable("trust proxy");
+
 app.use(cors);
+app.use(limiter);
 
 app.get("/address", handleAddressRequest);
 
