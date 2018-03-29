dbf2672585a04ea0f036a7e0a6bf33011a0e4f32
Felix Gladisch <felix@felixgladisch.de>
2018-03-29 10:34:36 +0200

    refactor: cors middlware typings

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -1,7 +1,9 @@
-export default (req, res, next) => {
+import { NextFunction, Request, Response } from "express";
+
+export default (req: Request, res: Response, next: NextFunction) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
   res.set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization");
 
-  return next();
+  next();
 };
